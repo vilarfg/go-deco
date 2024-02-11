d37a1ae453ab5464b16209f864701448ef576426
Fernando G. Vilar <ferv.git@gmail.com>
2024-02-11 21:46:17 +0700

    refactor: :fire: simplifies implementation

--- deco.go
+++ deco.go
@@ -19,35 +19,14 @@ func extend[T any](original Decorator[T], ds []Decorator[T]) Decorator[T] {
 			ds[i] = d
 		}
 	}
-	switch i {
-	case -1:
-		if original == nil {
-			return identity
-		}
-		return func(t T) T { return original(t) }
-	case 0:
-		d = ds[0]
-		if original == nil {
-			return func(t T) T { return d(t) }
-		}
-		return func(t T) T { return original(d(t)) }
-	}
-	if original == nil {
-		return func(t T) T {
-			var j = i
-			for ; j >= 0; j-- {
-				t = ds[j](t)
-			}
-			return t
-		}
-	}
 	return func(t T) T {
 		var j = i
 		for ; j >= 0; j-- {
 			t = ds[j](t)
 		}
+		if original == nil {
+			return t
+		}
 		return original(t)
 	}
 }
-
-func identity[T any](t T) T { return t }
